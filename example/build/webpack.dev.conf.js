0ba7311b642acdc71e252f03f8cbe6ee46b991c5
flyswifxjsu <flyswifxjsu@tencent.com>
2023-05-09 14:31:10 +0800

    接入代码

---
+++ example/build/webpack.dev.conf.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './example/app.js',
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'main.js'
+    },
+    devtool: 'cheap-module-source-map',
+    devServer: {
+        port: 8082,
+        disableHostCheck: true
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        modules: [
+            path.join(__dirname, '../'),
+            'node_modules'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: ['vue-style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'img/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'fonts/[name].[ext]'
+                }
+            },
+            {
+                test: /\.vue$/,
+                use: [{
+                    loader: 'vue-loader'
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './example/index.html',
+            inject: true
+        }),
+        new VueLoaderPlugin(),
+    ]
+}
